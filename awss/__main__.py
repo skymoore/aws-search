4de99d91d93327ff3662c96ca823c708f5d99a12
Sky Moore <sky.moore@polysign.io>
2023-06-02 20:59:35 +0400

    add ami instances to ec2

--- awss/__main__.py
+++ awss/__main__.py
@@ -2,7 +2,7 @@ import boto3
 from threading import Lock
 from logging import basicConfig, INFO, DEBUG, info
 from click import option, group, pass_context #, Choice
-from .lib import list_rds_instances
+from .lib import list_rds_instances, find_instances_by_ami_owner
 
 print_lock = Lock()
 
@@ -26,16 +26,33 @@ def cli(ctx, profile, workers, debug):
     info(f"using profile {ctx.obj['session'].profile_name} ({id_arn})")
 
 
-@cli.command()
+@cli.group(help="RDS commands")
 @pass_context
 def rds(ctx):
+    pass
+
+@rds.command("list", help="List all RDS instance and cluster names")
+@pass_context
+def list_instances(ctx):
     list_rds_instances(ctx.obj["session"], ctx.obj["workers"], print_lock)
 
-
-@cli.command()
+@cli.group(help="EC2 commands")
 @pass_context
 def ec2(ctx):
-    raise NotImplementedError
+    pass
+
+@ec2.command("ami-instances", help="Find ec2 instances running an ami owned by the specified owner ID where the ami name matches the specified filter")
+@pass_context
+# defaults find ubuntu 18.04 instances
+@option("--owner-id", "-o", help="AMI owner ID, example: '099720109477' (Canonical)")
+@option("--ami-name-filter", "-n", help="AMI name filter, to supply wildcards enclose the argument in single quotes, example: '*18.04*'")
+def ami_instances(ctx, owner_id, name_filter):
+    find_instances_by_ami_owner(
+        ctx.obj["session"],
+        ctx.obj["workers"],
+        owner_id,
+        name_filter,
+    )
 
 
 @cli.command()
--- awss/lib.py
+++ awss/lib.py
@@ -1,12 +1,12 @@
 import concurrent.futures
 from time import perf_counter
-from logging import info, debug
+from logging import info, debug, error, warning
 
 def get_regions(session, service):
     info("getting regions...")
     return session.get_available_regions(service)
 
-def list_rds_instances(session, workers, print_lock):
+def list_rds_instances(session, workers, info_lock):
     def list_rds(session, region):
         try:
             rds = session.client("rds", region_name=region)
@@ -24,7 +24,7 @@ def list_rds_instances(session, workers, print_lock):
                 ]
             )
 
-            with print_lock:
+            with info_lock:
                 if len(db_names) > 0:
                     info(
                         f"region: {region}\n\tfound {len(db_names)} databases:\n\t\t"
@@ -33,8 +33,8 @@ def list_rds_instances(session, workers, print_lock):
                 else:
                     info(f"no databases found in {region}")
         except Exception as e:
-            with print_lock:
-                debug(f"in {region}: {e}")
+            with info_lock:
+                warning(f"in {region}: {e}")
 
     start = perf_counter()
     regions = get_regions(session, "rds")
@@ -48,3 +48,84 @@ def list_rds_instances(session, workers, print_lock):
     
     end = perf_counter()
     info(f"list_rds_instances() execution time: {end - start:.2f}s")
+
+
+def get_ami_ids_by_owner(session, region, owner_id, name_filter):
+    ec2 = session.client("ec2", region_name=region)
+    ami_ids = {}
+    filter = [
+        {
+            "Name": "name",
+            "Values": [name_filter],
+        }
+    ]
+    owner_id = [owner_id]
+
+    response = ec2.describe_images(
+        Owners=owner_id,
+        Filters=filter,
+    )
+    for ami in response["Images"]:
+        ami_ids[ami["ImageId"]] = ""
+
+    while "NextToken" in response:
+        response = ec2.describe_images(
+            Owners=owner_id,
+            Filters=filter,
+            NextToken=response["NextToken"],
+        )
+        for ami in response["Images"]:
+            ami_ids[ami["ImageId"]] = ""
+
+    info(f"{len(ami_ids.keys())} amis owned by {owner_id[0]} in region {region}")
+    return ami_ids
+
+def find_instances_by_ami_owner(session, workers, owner_id, name_filter):
+    def find_instances(session, region, owner_id, name_filter):
+        try:
+            ec2_resource = session.resource("ec2", region_name=region)
+            instances = ec2_resource.instances.all()
+            num_instances = len(list(instances))
+
+            if num_instances == 0:
+                info(f"no ec2 instances found in region {region}")
+                return
+
+            info(f"{num_instances} ec2 instances in region {region}")
+            ami_ids = get_ami_ids_by_owner(session, region, owner_id, name_filter)
+
+            found_instance = False
+            matches = []
+            for instance in instances:
+
+                try:
+                    if instance.image_id in ami_ids:
+                        found_instance = True
+                        matches.append(instance.id)
+                        info(
+                            f"matching instance in region {region} with ID: {instance.id}"
+                        )
+                except Exception as e:
+                    error(f"{instance.id}: {e}")
+                    continue
+
+            if not found_instance:
+                info(f"no matching instances found in region {region}")
+            else:
+                info(f"{len(matches)} matching instances found in region {region}")
+
+        except Exception as e:
+            warning(f"{region}: {e}")
+    
+    start = perf_counter()
+    regions = get_regions(session, "ec2")
+    info(
+        f"searching {len(regions)} aws regions with {workers} workers for ec2 instances running an ami owned by {owner_id} whose name matches {name_filter}..."
+    )
+    inputs = [(session, region, owner_id, name_filter) for region in regions]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        executor.map(lambda params: find_instances(*params), inputs)
+    
+    end = perf_counter()
+    info(f"find_instances_by_ami_owner() execution time: {end - start:.2f}s")
